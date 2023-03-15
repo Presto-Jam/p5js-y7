2705da94ff48b3d93f4894656d5fb5158b46f0ec
King-click <kingstontsai@gmail.com>
2023-03-15 11:42:08 +0800

    Making grid generation

--- code files/p5.js code/art arsenal.js
+++ code files/p5.js code/art arsenal.js
@@ -1,14 +1,15 @@
 //resetting variables
 let squaresUpdated =  0
-let update = true
+let updatesqr = true
+let startbox = false
 
 //canvas variables. Recommended to have ratio of 1:1
 let canvasx = 480
 let canvasy = 480
 
 //grid variables
-let gridx = 10
-let gridy = 10
+let gridx = 4
+let gridy = 4
 let boxx = canvasx/gridx
 let boxy = canvasy/gridy
 
@@ -16,24 +17,35 @@ let boxy = canvasy/gridy
 let startboxx = 0
 let startboxy = 0
 
+let nowbox = []
+
 
 
 function setup() {
     createCanvas(480, 480);
-    noStroke()
+    //noStroke()
+
+    startboxx = Math.floor(random()*gridx)
+    startboxy = Math.floor(random()*gridy)
 }
 
 function draw() {
     //GENERATE THE GRID
     for(let gy = 0; gy < gridy; gy++) {
         for(let gx = 0; gx < gridx; gx++) {
+            fill(255,255,255)
             rect(gx*boxx, gy*boxy, boxx, boxy)
         }
     }
 
     //PROCEDURALLY GENERATING
     //generate a starting box
-    startboxx = Math.floor(random()*gridx)
-    startboxy = Math.floor(random()*gridy)
+    fill(0,0,0)
+    rect(startboxx*boxx,startboxy*boxy,boxx,boxy)
+
+    //update nowbox
+    nowbox.push(startboxx,startboxy)
+
+    //generate other boxes
 
 }
