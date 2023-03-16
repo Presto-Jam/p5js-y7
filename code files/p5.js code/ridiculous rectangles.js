a0443898dbf781d889ad6dddc6d4f8017fb3ef66
presto.py <65468855+Prestopy@users.noreply.github.com>
2023-03-16 15:54:49 +0800

    testing Instantiation for p5.js scripts

--- code files/p5.js code/ridiculous rectangles.js
+++ code files/p5.js code/ridiculous rectangles.js
@@ -1,26 +1,30 @@
+let sketch = function(p) {
 //preset variables
-let canvasx = 400; //canvas x length
-let canvasy = 400; //canvas y length
-let boxamt = 100; //amount of boxes on screen
-let boxsizex = canvasx/boxamt; //AUTO-CALC: box x length
+  let canvasx = 400; //canvas x length
+  let canvasy = 400; //canvas y length
+  let boxamt = 100; //amount of boxes on screen
+  let boxsizex = canvasx/boxamt; //AUTO-CALC: box x length
 
-let maxred = 20; //maximum RED amount in RGB color randomization
-let maxgreen = 230; //maximum GREEN amount in RGB color randomization
-let maxblue = 180; //maximum BLUE amount in RGB color randomization
+  let maxred = 20; //maximum RED amount in RGB color randomization
+  let maxgreen = 230; //maximum GREEN amount in RGB color randomization
+  let maxblue = 180; //maximum BLUE amount in RGB color randomization
 
 
 
-function setup() {
-  let rirecnv = createCanvas(canvasx, canvasy); //creates a canvas
+  p.setup = function() {
+    let rirecnv = createCanvas(canvasx, canvasy); //creates a canvas
 
-  //HTML Setup
-  rirecnv.parent('RiRe')
-}
-
-function draw() {
-  for(let i = 0; i < boxamt; i++) {
-      fill(random()*maxred, random()*maxgreen, random()*maxblue);
-      strokeWeight(0);
-      rect(i*boxsizex, 0, boxsizex,random()*canvasy);
+    //HTML Setup
+    rirecnv.parent('RiRe')
   }
+
+  p.draw = function() {
+    for(let i = 0; i < boxamt; i++) {
+        fill(random()*maxred, random()*maxgreen, random()*maxblue);
+        strokeWeight(0);
+        rect(i*boxsizex, 0, boxsizex,random()*canvasy);
+    }
+  }
+
+  let canvas_rire = new p5(sketch)
 }
