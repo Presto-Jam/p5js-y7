d0b527880438f1def9916ddbc1692c6854d8d8f0
presto.py <65468855+Prestopy@users.noreply.github.com>
2023-03-18 07:33:42 +0800

    added an event listener to .window

--- portfolio/cursor.js
+++ portfolio/cursor.js
@@ -1,10 +1,11 @@
 let circle = document.getElementById('custom-cursor');
-const onMouseMove = (e) =>{
+const onMouseMove = (e) => {
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + 'px';
 }
-window.onscroll = function (e) {
+const onscroll = (e) => {
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + 'px';
 }
 document.addEventListener('mousemove', onMouseMove);
+window.addEventListener('scroll', onscroll);
