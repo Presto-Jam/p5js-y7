ab1e60f4d60273d53b52966fa22442bdb1086329
presto.py <65468855+Prestopy@users.noreply.github.com>
2023-03-17 12:24:48 +0800

    testing

--- portfolio/cursor.js
+++ portfolio/cursor.js
@@ -1,6 +1,6 @@
 let circle = document.getElementById('custom-cursor');
 const onMouseMove = (e) =>{
-  circle.style.left = e.mouseX + 'px';
-  circle.style.top = e.mouseY + 'px';
+  circle.style.left = e.pageX + 'px';
+  circle.style.top = e.pageY + 'px';
 }
 document.addEventListener('mousemove', onMouseMove);
