8805b156b56a639a277e07e9482eb1996ffb2251
presto16 <65468855+King-click@users.noreply.github.com>
2023-03-18 07:19:12 +0800

    onScroll: cursor follow

--- portfolio/cursor.js
+++ portfolio/cursor.js
@@ -3,4 +3,8 @@ const onMouseMove = (e) =>{
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + 'px';
 }
+window.onscroll = function (e) {
+  circle.style.left = e.pageX + 'px';
+  circle.style.top = e.pageY + 'px';
+}
 document.addEventListener('mousemove', onMouseMove);
