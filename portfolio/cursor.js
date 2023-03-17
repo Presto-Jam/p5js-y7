6a8c54a1f85fa51b6cb825309c50f3675a39b1fb
presto.py <65468855+Prestopy@users.noreply.github.com>
2023-03-18 07:46:48 +0800

    accounted for window.scrollY

--- portfolio/cursor.js
+++ portfolio/cursor.js
@@ -1,11 +1,6 @@
 let circle = document.getElementById('custom-cursor');
 const onMouseMove = (e) => {
   circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + 'px';
-}
-const onscroll = (e) => {
-  circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + 'px';
+  circle.style.top = e.pageY + window.scrollY + 'px';;
 }
 document.addEventListener('mousemove', onMouseMove);
-document.addEventListener('scroll', onscroll);
