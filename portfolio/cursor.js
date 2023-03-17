1e295122ec289a41968e0bd6b867ea133f3a0fe1
presto.py <65468855+Prestopy@users.noreply.github.com>
2023-03-18 07:54:15 +0800

    improved scaling with js

--- portfolio/cursor.js
+++ portfolio/cursor.js
@@ -1,6 +1,14 @@
 let circle = document.getElementById('custom-cursor');
+
 const onMouseMove = (e) => {
   circle.style.left = e.pageX + 'px';
   circle.style.top = e.pageY + window.scrollY + 'px';;
 }
 document.addEventListener('mousemove', onMouseMove);
+
+// interactiveness
+let projects = document.getElementsByClassName('project-container')
+
+projects.addEventListener('mouseenter', () => {
+  circle.style.scale = 105%;
+})
