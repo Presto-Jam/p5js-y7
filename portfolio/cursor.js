db4a7ff6ab0db93ce5fec01ebfd3671fc0aad18f
presto.py <65468855+Prestopy@users.noreply.github.com>
2023-03-17 13:00:03 +0800

    reverted changes

--- portfolio/cursor.js
+++ portfolio/cursor.js
@@ -1,11 +1,6 @@
 let circle = document.getElementById('custom-cursor');
-circle.style.left = e.mouseX + 'px';
-circle.style.top = e.mouseY + 'px';
-
-document.addEventListener('mousemove', e => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-
-  gsap.to(cursor, 0.5, {x: mouseX, y:mouseY})
-})
+const onMouseMove = (e) =>{
+  circle.style.left = e.pageX + 'px';
+  circle.style.top = e.pageY + 'px';
+}
 document.addEventListener('mousemove', onMouseMove);
