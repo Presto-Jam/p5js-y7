4562f470c3f879a47f30815d63fdcfa298f9f15b
presto.py <65468855+Prestopy@users.noreply.github.com>
2023-03-18 08:06:06 +0800

    scrolling???

--- portfolio/cursor.js
+++ portfolio/cursor.js
@@ -2,19 +2,10 @@ let circle = document.getElementById('custom-cursor');
 
 const onMouseMove = (e) => {
   circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + window.scrollY + 'px';;
+  circle.style.top = e.pageY + 'px';;
 }
+document.body.addEventListener("scroll", e => {
+  root.style.setProperty('--mouse-x', e.clientX + "px");
+  root.style.setProperty('--mouse-y', window.scrollY + e.clientY + "px");
+});
 document.addEventListener('mousemove', onMouseMove);
-
-// interactiveness
-let projects = document.getElementsByClassName('project-container')
-
-projects.addEventListener('mouseenter', () => {
-  gsap.to(cursor, 1, {
-      scale: 1,
-      opacity: 1,
-      top: '-75px',
-      left: '-75px',
-      ease: Elastic.easeOut.config(1, 0.3)
-  })
-})
