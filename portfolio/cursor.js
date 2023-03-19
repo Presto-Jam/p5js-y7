6f06063f832b4304907d79f6fda4eca98d5c5932
King-click <kingstontsai@gmail.com>
2023-03-19 11:00:57 +0800

    fixed cursor attributes with scrolling
    Signed-off-by: King-click <kingstontsai@gmail.com>

--- portfolio/cursor.js
+++ portfolio/cursor.js
@@ -2,10 +2,10 @@ let circle = document.getElementById('custom-cursor');
 
 const onMouseMove = (e) => {
   circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + 'px';;
+  circle.style.top = e.pageY + 'px';
 }
 document.body.addEventListener("scroll", e => {
-  root.style.setProperty('--mouse-x', e.clientX + "px");
-  root.style.setProperty('--mouse-y', e.clientY + "px");
+  circle.style.left = e.pageX + "px";
+  circle.style.top = e.pageY + "px";
 });
 document.addEventListener('mousemove', onMouseMove);
