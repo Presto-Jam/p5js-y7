d1f881312067b74b998f962db2687b78fe5d2a25
King-click <kingstontsai@gmail.com>
2023-03-20 13:41:18 +0800

    variations
    Signed-off-by: King-click <kingstontsai@gmail.com>

--- code/p5/perplexing_parallax.js
+++ code/p5/perplexing_parallax.js
@@ -7,34 +7,49 @@ let y1 = 250
 let y2 = 300
 let y3 = 450
 
+//colors
+let box1c
+let box2c
+let box3c
+
 function setup() {
     createCanvas(500, 500);
+    noStroke()
+
+    box1c = [random()*255,random()*255,random()*255]
+    box2c = [random()*255,random()*255,random()*255]
+    box3c = [random()*255,random()*255,random()*255]
 }
 
 function draw() {
-    let pmx = mouseX * 0.3
-    let pmy = mouseY * 0.2
+    ellipse()
+    let pmx1 = mouseX * 0.3
+    let pmx2 = mouseX * 0.4
+    let pmx3 = mouseX * 0.2
+    let pmy1 = mouseY * 0.2
+    let pmy2 = mouseY * 0.3
+    let pmy3 = mouseY * 0.6
 
 
     //update parallax effects
     addEventListener("mousemove", (event) => {});
 
     onmousemove = (event) => {
-        x1 = -pmx + 500
-        x2 = -pmx + 500
-        x3 = -pmx + 500
-        y1 = 250 - pmy
-        y2 = 250 - pmy
-        y3 = 250 - pmy
+        x1 = -pmx1 + 250
+        x2 = -pmx2 + 300
+        x3 = -pmx3 + 400
+        y1 = 250 - pmy1
+        y2 = 300 - pmy2
+        y3 = 400 - pmy3
     };
 
     background(0,0,0);
-    fill(255,255,255)
+    fill(box1c[0],box1c[1],box1c[2])
     rect(x1,y1,50,50)
 
-    fill(255,255,255)
+    fill(box2c[0],box2c[1],box2c[2])
     rect(x2,y2,50,50)
 
-    fill(255,255,255)
+    fill(box3c[0],box3c[1],box3c[2])
     rect(x3,y3,50,50)
 }
